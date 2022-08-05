09b1125d794c19b8fe72fedb1323905b711252d2
Chiwen.L <zxcvbnm840508@gmail.com>
2022-08-05 18:12:40 +0900

    remove api key

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,6 @@ import LeftControl from './components/LeftControl';
 import RightControl from './components/RightControl';
 import MainView from './components/MainView';
 
-const key = '988299740f950d31d917c48e11e92c76';
-const city = '';
-const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`;
-
 function App() {
   return (
     <>
